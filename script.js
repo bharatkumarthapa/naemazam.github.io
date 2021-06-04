b006179206f169907903900bae39c2bfa419e492
Naem Azam <78720392+naemazam@users.noreply.github.com>
2021-06-04 15:42:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Tech Specialist ", "Linux Admin", "Developer", "Blogger", "Designer", "programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Tech Specialist ", "Linux Admin", "Developer", "Blogger", "Designer", "programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    
